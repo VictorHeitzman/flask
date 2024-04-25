4137a68851da731288d50a441c4cfc48cc2ac14a
Victor Heitzman <heitzmam@gmail.com>
2024-04-25 15:08:52 -0300

    Alterando arquivo main

--- jogoteca.py
+++ jogoteca.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_wtf.csrf import CSRFProtect
 from flask_bcrypt import Bcrypt
+from config import os
 
 app = Flask(__name__)
 
@@ -17,4 +18,5 @@ from views_user import *
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port =  int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
